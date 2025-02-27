149d210cc3ec61475458e10aa4f5a1d271ad9574
Jon Robeson <jcbrobeson@gmail.com>
2025-02-27 01:02:28 -0600

    adding variants back in for entrance animation and reordering animate presence

--- src/components/DataTableBody.tsx
+++ src/components/DataTableBody.tsx
@@ -1,12 +1,13 @@
 import { type ReactNode } from 'react';
 import { type DataTableProps as DataTableBodyProps } from './DataTable';
-import { motion, AnimatePresence } from 'motion/react';
+import { motion, AnimatePresence, useIsPresent } from 'motion/react';
 
 interface WithOptionalId {
 	id?: string | number;
 }
 
 export default function DataTableBody<T>({ data, columns, rowProps }: DataTableBodyProps<T>) {
+	const isPresent = useIsPresent();
 	if (!data || data.length === 0) {
 		return (
 			<div className='overflow-x-auto max-h-[45rem]'>
@@ -27,22 +28,34 @@ export default function DataTableBody<T>({ data, columns, rowProps }: DataTableB
 		const { className, ...rest } = additionalProps;
 		const key = (row as WithOptionalId).id || rowIndex;
 		return (
-			<AnimatePresence key={key}>
-				<motion.tr {...rest} layout className={`hover:bg-gray-100 hover:text-gray-900 ${className || ''}`}>
-					{columns.map((col, colIndex) => (
-						<td key={colIndex} className='px-6 py-3 whitespace-nowrap text-sm font-medium'>
-							{col.Cell ? col.Cell(row) : typeof col.accessor === 'function' ? col.accessor(row) : (row[col.accessor] as ReactNode)}
-						</td>
-					))}
-				</motion.tr>
-			</AnimatePresence>
+			<motion.tr
+				key={key}
+				{...rest}
+				layout
+				initial='out'
+				animate={isPresent ? 'in' : 'out'}
+				variants={{
+					in: { scaleY: 1, opacity: 1 },
+					out: { scaleY: 0, opacity: 0 },
+				}}
+				transition={{ type: 'spring', stiffness: 500, damping: 50, mass: 1 }}
+				className={`hover:bg-gray-100 hover:text-gray-900 ${className || ''}`}
+			>
+				{columns.map((col, colIndex) => (
+					<td key={colIndex} className='px-6 py-3 whitespace-nowrap text-sm font-medium'>
+						{col.Cell ? col.Cell(row) : typeof col.accessor === 'function' ? col.accessor(row) : (row[col.accessor] as ReactNode)}
+					</td>
+				))}
+			</motion.tr>
 		);
 	});
 
 	return (
 		<div className='overflow-x-auto max-h-[45rem]'>
 			<table className='min-w-full'>
-				<tbody className='bg-white divide-y divide-gray-200'>{content}</tbody>
+				<AnimatePresence>
+					<tbody className='bg-white divide-y divide-gray-200'>{content}</tbody>
+				</AnimatePresence>
 			</table>
 		</div>
 	);
